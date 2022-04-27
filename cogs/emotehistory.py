aa7e20e74330447eff70aa5e142eb13b03a6a346
mille5a9 <andrewm192@outlook.com>
2022-04-27 18:25:48 +0000

    tally-everything except channels that it cant read

--- cogs/emotehistory.py
+++ cogs/emotehistory.py
@@ -106,6 +106,45 @@ class EmoteHistory(commands.Cog):
 
         msg = await ctx.send(self.printDict(dict(sorted(emote_dict.items(), key=lambda x: x[1], reverse=True))))
         await ctx.message.remove_reaction(constants.AFFIRMATIVE_REACTION_EMOJI, msg.author)
+        
+    @commands.command(name='tally-everything')
+    @commands.bot_has_permissions(read_message_history=True)
+    @commands.is_owner()
+    async def tally_everything(self, ctx, after, endRange=None):
+        """
+        Sums all custom emote usage and reactions for all messages in all channels *after* a given date. Can limit the date range with an optional end date. Dates must be %m/%d/%y (say, 4-25-22)
+        """
+        
+        await ctx.message.add_reaction(constants.AFFIRMATIVE_REACTION_EMOJI)
+
+        emote_dict = {}
+        after_date = datetime.strptime(after, '%m-%d-%y')
+        before_date = None if endRange is None else pytz.utc.localize(datetime.strptime(after, '%m-%d-%y'))
+        for emote in ctx.guild.emojis:
+            emote_dict[emote.name] = 0
+
+        # crawl by channel
+        for channel in ctx.guild.text_channels:
+            if channel.permissions_for(ctx.me).read_message_history == False: continue
+            async for message in channel.history(limit=sys.maxsize, before=before_date, after=pytz.utc.localize(after_date)):
+                # examine reactions
+                print(str(message.created_at) + ' - ' + message.content)
+                for reaction in message.reactions:
+                    if isinstance(reaction.emoji, str):
+                        if reaction.emoji in emote_dict:  emote_dict[reaction.emoji] += reaction.count 
+                    else: 
+                        if reaction.emoji.name in emote_dict: emote_dict[reaction.emoji.name] += reaction.count
+                
+                # split message on colons and compare the 2nd - n-1th elements looking for dict entries to increment
+                splitted = message.content.split(':')
+                length = len(splitted)
+                for i in range(length):
+                    if i == 0 or i == length - 1: continue
+                    if splitted[i] in emote_dict: emote_dict[splitted[i]] += 1
+
+        msg = await ctx.send(self.printDict(dict(sorted(emote_dict.items(), key=lambda x: x[1], reverse=True))))
+        await ctx.message.remove_reaction(constants.AFFIRMATIVE_REACTION_EMOJI, msg.author)
+
             
 def setup(bot):
     bot.add_cog(EmoteHistory(bot))
