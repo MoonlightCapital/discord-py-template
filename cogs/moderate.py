68dc7df97063d63243af276507ea569d7b83ad0c
mille5a9 <andrewm192@outlook.com>
2022-06-23 19:01:12 +0000

    refactored moderation cog

--- cogs/moderate.py
+++ cogs/moderate.py
@@ -1,12 +1,19 @@
+import sys
 import nextcord as discord
 import re
 from nextcord.ext import commands
 
-from internal import clear, constants
-
 class Moderate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    
+    async def reprimand_offender(self, user: discord.User, reason=''):
+        reprimand = self.bot.get_cog('Reprimand')
+        if reprimand is not None:
+            await reprimand.process_reprimand(user, reason)
+        else:
+            print('Could not reprimand offender, self.bot.get_cog(\'Reprimand\' is None.')
             
     @commands.Cog.listener("on_message")
     async def check_for_links(self, message):
@@ -15,24 +22,42 @@ class Moderate(commands.Cog):
         """
         if message.author.bot: return  # ignore all bots
         if message.channel.name != 'babe-wake-up': # ignore messages that aren't top-level and in babe-wake-up
-            print('Wrong channel, skipping message listener...\n')
+            print('Moderate.py: Wrong channel, skipping message listener...\n')
             return
-            
-        print('reading message for link...')
+        if message.thread is not None: 
+            print('Moderate.py: Thread created, skipping message listener...\n')
+            return # ignore messages that create threads
 
-        #regexLinkPattern = re.compile('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
+        print('reading message for link...')
         link = re.search('((http|https)\:\/\/)[a-zA-Z0-9\.\/\?\:\-_=#][a-zA-Z0-9\.\/\?\:\-_=#]+', message.content)
-        #link = regexLinkPattern.search(message.content)
 
         if link is None: # act upon linkless message
-            
-            msg = await message.channel.send('' + message.author.mention + ' this message does not contain any links. If this message relates to a previously linked video, please consider making a threaded response to that video. This notification will self-destruct when you move your message.')
-            
-            # delete message after 2 minutes or if clear reaction is clicked, whichever comes first
-            await clear.clear_on_message_deleted(self.bot, msg, message)
+            msg = await message.reply('This message does not contain any link(s); if it relates to previously linked media, please consider making a threaded response to the original post. You have been reprimanded, and this notification will self-destruct when you move your message.')
+            await self.reprimand_offender(message.author, 'Moderator: #babe-wake-up rule violation.')
 
         else:
             print('message has link as expected')
+
+        
+    @commands.bot_has_permissions(read_message_history=True)
+    @commands.Cog.listener("on_message_delete")
+    async def clean_up_channel(self, deletedMessage: discord.Message):
+        """
+        This event checks every deleted message in the media channel for replies from itself to remove / clean up. 
+        """
+        if deletedMessage.author.bot: return  # ignore all bots
+        if deletedMessage.channel.name != 'babe-wake-up': # ignore messages that aren't top-level and in babe-wake-up
+            print('Moderate.py: Wrong channel, skipping message deleted listener...\n')
+            return
+        
+        # find the reply to the deleted message and clean it up
+        async for message in deletedMessage.channel.history(limit=sys.maxsize, before=None, after=deletedMessage.created_at):
+            if (message.author.id == self.bot.user.id) and (message.reference.message_id == deletedMessage.id):
+                await message.delete()
+                return 
+                # there will be only one message to remove
+                # it is likely to be the very next message in the history, so this method minimizes iteration
+    
 
 
 def setup(bot):
